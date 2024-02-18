7ee41cd7ade0874ce88a40427725526780a69f0b
azamatif <azamatif@code.edu.az>
2024-02-18 12:44:57 +0100

    #added arrays home task

---
+++ ArraysHomeTask/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Hello, World!");
+#region
+////arrayin elementlerinin cemi
+//int[] nums = { 4, 7, 9, 13, 2, 19, 21, 15 };
+//int sum = 0;
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    sum += nums[i];
+//}
+//Console.WriteLine(sum);
+#endregion
+
+#region
+//arrayin ilk ve son elementlerinin cemi
+//int[] nums = { 2, 4, 6, 8, 12, 18, 22, 26, 30 };
+//int sum = nums[0] + nums[nums.Length-1];
+//Console.WriteLine(sum);
+
+#endregion
+
+#region
+//arrayin en boyuk deyere sahip elementi
+//int[] nums = { 5, 7, 9, 11, 23, 15, 29, 17, 19};
+//int largest = 0;
+
+//for (int i = 1; i < nums.Length; i++)
+//{
+//	if ( nums[i]> largest )
+//	{
+//		largest = nums[i];
+//	}
+//}
+//Console.WriteLine("En boyuk eded:"+largest);
+
+#endregion
